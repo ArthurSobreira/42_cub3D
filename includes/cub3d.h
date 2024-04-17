a92686378a1f453bebab24aab8d300a718dc4743
Arthur Sobreira <tutusobreira@gmail.com>
2024-04-17 12:32:23 -0300

    Add main header

---
+++ includes/cub3d.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/17 12:22:34 by arsobrei          #+#    #+#             */
+/*   Updated: 2024/04/17 12:27:58 by arsobrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "../libs/libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <math.h>
+# include <stdio.h>
+# include <string.h>
+
+
+#endif
