8d5e2c63438b2499acf0d664e79758ed4df4b346
Arthur Sobreira <tutusobreira@gmail.com>
2024-04-22 12:10:29 -0300

    Update main header

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 12:22:34 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/04/22 12:04:03 by arsobrei         ###   ########.fr       */
+/*   Updated: 2024/04/22 12:09:03 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,9 +57,11 @@ typedef struct s_cub3d
 	t_bool		error;
 }				t_cub3d;
 
+t_cub3d			*get_core(void);
+t_map			*get_map(void);
+
 void			set_max_x(size_t x);
 void			set_max_y(size_t y);
-t_map			*get_map(void);
 size_t			maxcol(void);
 int				lenfd(void);
 void			valid_open_map(void);
@@ -67,6 +69,7 @@ void			ft_print_map(void);
 void			ft_error(char *msg);
 void			constructor_map(void);
 // void			fill_with_spaces(void);
+t_bool			valid_map_name(char *map_name);
 char			**get_parser_infos(t_cub3d *core);
 t_bool			is_map(char *line);
 
