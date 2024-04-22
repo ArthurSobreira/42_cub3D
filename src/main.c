7b95f4acb86202af95d158ae16ff8e56a3a2ca4d
phenriq2 <phenriq2@student.42sp.org.br>
2024-04-22 11:46:44 -0300

    restart main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 12:22:14 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/04/19 19:41:49 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/04/22 11:15:02 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,25 @@ void	build_map(void)
 
 	i = 0;
 	map = get_map();
-	printf("max_y: %d\n", map->max_y);
 	fd = open("./maps/map1.cub", O_RDONLY);
+	map->map_str = malloc(sizeof(char *) * map->max_y);
+	while (i < map->max_y)
+	{
+		map->map_str[i] = malloc(sizeof(char) * map->max_x);
+		i++;
+	}
+	i = 0;
 	while (i < map->max_y - 1)
 	{
 		line = get_next_line(fd);
-		line[ft_strlen(line) - 1] = ' ';
+		line[ft_strlen(line) - 1] = '\0';
 		ft_memcpy(map->map_str[i], line, ft_strlen(line));
-		printf("line: [%s]\n", map->map_str[i]);
 		i++;
 		free(line);
 	}
+	line = get_next_line(-1);
+	map->map_str[lenfd() - 1] = NULL;
+	close(fd);
 }
 
 int	main(void)
@@ -44,7 +52,8 @@ int	main(void)
 	map = get_map();
 	set_max_y(lenfd());
 	set_max_x(maxcol());
-	printf("max_x: %d\n", map->max_x);
-	printf("max_y: %d\n", map->max_y);
+	build_map();
+	ft_print_map();
+	ft_free_matrix(map->map_str);
 	return (0);
 }
