004602b6f41b7f1fb821d91fadd19991c479e06a
Arthur Sobreira <tutusobreira@gmail.com>
2024-04-29 17:29:37 -0300

    Update gnl sources

--- libs/libft/get_next_line/includes/get_next_line.h
+++ libs/libft/get_next_line/includes/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 11:32:49 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/01/18 17:02:06 by arsobrei         ###   ########.fr       */
+/*   Updated: 2024/04/29 14:30:37 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,22 +24,12 @@
 #  define CLEAR_STATIC -1
 # endif
 
-// Main Struct
-typedef struct s_gnl
-{
-	char			data;
-	struct s_gnl	*next;
-}					t_gnl;
-
 // Functions Prototypes
 char	*get_next_line(int fd);
-int		ft_sizelst(t_gnl **list_of_char, char c);
-int		ft_read_error(t_gnl **main_list, char *current);
-void	ft_start_gnl(t_gnl **caracter, char c);
-void	ft_wipe_list(t_gnl **list_of_char, int counter);
-int		ft_read_the_file(int fd, t_gnl **main_list);
-int		ft_split_char(const char *c, t_gnl **main_list);
-void	ft_add_caracter(t_gnl **main_list, char character);
-void	ft_extract_line(t_gnl **list_of_char, char **line, int counter);
+size_t	ft_strlen_gnl(const char *s);
+char	*ft_strncpy_gnl(char *dest, const char *src, size_t n);
+char	*ft_strchr_gnl(const char *s, int c);
+char	*ft_strdup_gnl(const char *s);
+char	*ft_strjoin_gnl(char *s1, char *s2);
 
 #endif
