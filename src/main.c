00c1f2f37f86ba25245d4b0841e7ba9b9ceca4c5
phenriq2 <phenriq2@student.42sp.org.br>
2024-04-30 16:40:11 -0300

    exit function closing all files and returning the used memory

--- src/main.c
+++ src/main.c
@@ -1,16 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main2.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 12:22:14 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/04/23 17:51:31 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/04/30 10:45:22 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+static void	exit_program(void)
+{
+	ft_free_matrix(get_map()->map_str);
+	exit(EXIT_SUCCESS);
+}
 
 int	main(int argc, char *argv[])
 {
@@ -20,11 +26,11 @@ int	main(int argc, char *argv[])
 	core->map_path = argv[1];
 	if (argc != 2 || !valid_map_name(core->map_path))
 		ft_error(ERROR_INVALID_MAP_PATH);
-	// get_parser_infos(core);
 	constructor_map();
 	map_builder();
 	map_validation();
-	ft_free_matrix(get_map()->map_str);
+	exit_program();
 	return (EXIT_SUCCESS);
 }
+	// get_parser_infos(core);
 // continua no proximo episodio
