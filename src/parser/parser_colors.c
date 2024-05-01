e48c465aa76e685b6456add5ec5e203e5041491c
Arthur Sobreira <tutusobreira@gmail.com>
2024-04-30 22:55:04 -0300

    Update parser_colors.c

---
+++ src/parser/parser_colors.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser_colors.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/29 17:59:32 by arsobrei          #+#    #+#             */
+/*   Updated: 2024/04/29 17:59:56 by arsobrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+// void	parser_color(char **s_line, char **parser_infos, char \
+// 	*parser_checker[PARSER_INFOS_LEN])
+// {
+// 	char	*identifier;
+// 	char	*texture;
+
+// 	identifier = s_line[0];
+// 	texture = s_line[1];
+	
+// }
