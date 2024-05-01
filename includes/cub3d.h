af0dad91052b025f6e4c82a2ea6897c1fcef4b8e
Arthur Sobreira <tutusobreira@gmail.com>
2024-04-30 22:55:30 -0300

    Update main header

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 12:22:34 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/04/25 12:23:44 by arsobrei         ###   ########.fr       */
+/*   Updated: 2024/04/30 22:50:03 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,9 @@
 # include <string.h>
 # include <unistd.h>
 
-# define ERROR_INVALID_MAP_PATH "Error\nInvalid Map Path :(\n"
-# define ERROR_INCOMPLETE_MAP "Error\nIncomplete Map File :(\n"
+# define ERROR_INVALID_MAP_PATH "\033[0;31mError\nInvalid Map Path :(\033[0;31m\n"
+# define ERROR_INCOMPLETE_MAP "\033[0;31mError\nIncomplete Map File :(\033[0;31m\n"
+# define ERROR_INVALID_TEXTURE "\033[0;31mError\nInvalid Texture :(\033[0;31m\n"
 # define PERMITED_MAP_CHARS " 01NSEW"
 # define PARSER_INFOS_LEN 7
 # define TEXTURES_LEN 4
@@ -45,7 +46,7 @@ typedef	enum	e_colors
 {
 	CEILING = 4,
 	FLOOR
-}	t_textures;
+}	t_colors;
 
 typedef struct s_map
 {
@@ -61,7 +62,9 @@ typedef struct s_cub3d
 	t_map		*map;
 	char		*map_path;
 	char		*temp_map_path;
-	char 		**parser_infos;
+	short		texture_count;
+	short		color_count;
+	char 		*parser_infos[PARSER_INFOS_LEN];
 	uint32_t	colors[COLORS_LEN];
 	t_bool		error;
 }				t_cub3d;
@@ -79,10 +82,16 @@ void			ft_error(char *msg);
 void			constructor_map(void);
 // void			fill_with_spaces(void);
 
-char			**get_parser_infos(void);
+void			get_parser_infos(void);
 t_bool			valid_map_name(char *map_name);
 t_bool			is_map(char *line);
 t_bool			is_texture(char *line);
 t_bool			is_color(char *line);
+void			normalize_element(char *start);
+short			parser_texture(char **s_line, char **parser_infos);
+
+void	ft_free_local_matrix(char **matrix);
+void	clear_all(void);
+void	print_parser_infos(char **parser_infos);
 
 #endif
