9f4fccf9b9d4907aaceac44a54dab354fda3181d
phenriq2 <phenriq2@student.42sp.org.br>
2024-05-01 18:37:52 -0300

    fixing the main

--- src/main.c
+++ src/main.c
@@ -1,20 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main2.c                                            :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 12:22:14 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/04/30 10:45:22 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/05/01 18:30:53 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+static void	ft_free_map(void)
+{
+	int	i;
+
+	i = 0;
+	while (get_map()->map_str[i])
+	{
+		if (get_map()->map_str[i])
+			printf("%s\n", get_map()->map_str[i]);
+		free(get_map()->map_str[i]);
+		i++;
+	}
+}
+
 static void	exit_program(void)
 {
-	ft_free_matrix(get_map()->map_str);
+	ft_free_map();
+	// mlx_terminate(get_mlx()->mlx);
 	exit(EXIT_SUCCESS);
 }
 
@@ -27,10 +42,13 @@ int	main(int argc, char *argv[])
 	if (argc != 2 || !valid_map_name(core->map_path))
 		ft_error(ERROR_INVALID_MAP_PATH);
 	constructor_map();
-	map_builder();
+	constructor_gnl();
+	// map_builder();
 	map_validation();
+	ft_print_map();
+	// mlx_teste();
 	exit_program();
 	return (EXIT_SUCCESS);
 }
-	// get_parser_infos(core);
+// get_parser_infos(core);
 // continua no proximo episodio
