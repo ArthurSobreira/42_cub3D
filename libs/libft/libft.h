9298926f5cac0a5a6ac6570df4734ab68cda12e2
phenriq2 <phenriq2@student.42sp.org.br>
2024-05-02 12:57:41 -0300

    adding lenfd lstrip and rstrip functions to libft

--- libs/libft/libft.h
+++ libs/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 19:05:43 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/05/01 17:11:58 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/05/02 12:36:28 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -144,5 +144,6 @@ char					*ft_replace_dquotes(char *str, char *old, char *new);
 t_bool					is_bigger(char *s1, char *s2);
 char					*ft_lstrip(char *str);
 char					*ft_rstrip(char *str);
+int						ft_lenfd(int fd);
 
 #endif
--- libs/libft/libft/src/ft_lenfd.c
+++ libs/libft/libft/src/ft_lenfd.c
@@ -1,26 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_rstrip.c                                        :+:      :+:    :+:   */
+/*   ft_lenfd.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/01 16:09:14 by phenriq2          #+#    #+#             */
-/*   Updated: 2024/05/02 11:34:50 by phenriq2         ###   ########.fr       */
+/*   Created: 2024/05/02 12:05:17 by phenriq2          #+#    #+#             */
+/*   Updated: 2024/05/02 12:06:17 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_rstrip(char *str)
+int	ft_lenfd(int fd)
 {
-	ssize_t	i;
+	char	*line;
+	int		len;
 
-	i = ft_strlen(str) - 1;
-	while (i >= 0 && ft_isspace(str[i]))
+	len = 0;
+	while (TRUE)
 	{
-		str[i] = '\0';
-		i--;
+		line = get_next_line(fd);
+		if (!line)
+			break ;
+		len++;
+		free(line);
 	}
-	return (str);
+	get_next_line(-1);
+	return (len);
 }
