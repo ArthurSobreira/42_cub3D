18910b3bd006c32abbdeb22b0015ba0dd11d0c9e
Arthur Sobreira <tutusobreira@gmail.com>
2024-05-02 17:23:05 -0300

    Update parser_textures.c

--- src/parser/parser_textures.c
+++ src/parser/parser_textures.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 17:44:04 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/05/02 13:09:13 by arsobrei         ###   ########.fr       */
+/*   Updated: 2024/05/02 17:21:24 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@ static t_bool	match_texture(char *identifier, t_textures match, \
 	if (ft_strcmp(identifier, parser_checker[match]) == 0)
 	{
 		parser_checker[match] = NULL;
+		(*texture_count)++;
 		if (*texture_count > TEXTURES_LEN)
 			return (FALSE);
-		*texture_count += 1;
 		return (TRUE);
 	}
 	return (FALSE);
@@ -48,6 +48,11 @@ short	parser_texture(char **s_line, char **parser_infos)
 			parser_infos[WEST] = ft_strdup(texture);
 		else if (match_texture(identifier, EAST, &texture_count))
 			parser_infos[EAST] = ft_strdup(texture);
+		else
+		{
+			get_core()->bad_flag = TRUE;
+			ft_putstr_fd(ERROR_INVALID_TEXTURE, 2);
+		}
 	}
 	else
 		ft_error(ERROR_INVALID_TEXTURE);
