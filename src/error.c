dc49a9635c2852ba7e6bde6e06535527af68fa16
Arthur Sobreira <tutusobreira@gmail.com>
2024-05-07 16:17:07 -0300

    Update error.c

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 11:02:07 by phenriq2          #+#    #+#             */
-/*   Updated: 2024/04/30 22:56:09 by arsobrei         ###   ########.fr       */
+/*   Updated: 2024/05/02 17:21:01 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@ void	ft_error(char *msg)
 	t_cub3d	*core;
 
 	core = get_core();
-	ft_putstr_fd(msg, 2);
+	
+	if (!core->bad_flag)
+		ft_putstr_fd(msg, STDERR_FILENO);
 	clear_all();
 	exit(EXIT_FAILURE);
 }
