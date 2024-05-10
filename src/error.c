12d27bfb9a41002a3f6439d0504866364670f6ae
phenriq2 <phenriq2@student.42sp.org.br>
2024-05-10 17:58:18 -0300

    small repair

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 11:02:07 by phenriq2          #+#    #+#             */
-/*   Updated: 2024/05/07 17:33:07 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/05/10 17:32:34 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,6 @@ void	ft_error(char *msg)
 	core = get_core();
 	if (!core->bad_flag)
 		ft_putstr_fd(msg, STDERR_FILENO);
-	if (get_map()->map_str)
-		ft_free_matrix(get_map()->map_str);
 	clear_all();
 	exit(EXIT_FAILURE);
 }
