33962d75a97118df44131d360c3c553b99af6ca9
phenriq2 <phenriq2@student.42sp.org.br>
2024-05-10 17:59:42 -0300

    leaving the commented color function to do the testing

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 12:22:14 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/05/10 16:29:52 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/05/10 17:47:10 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,12 +32,13 @@ int	main(int argc, char *argv[])
 	core->map_path = argv[1];
 	if (argc != 2 || !valid_map_name(core->map_path))
 		ft_error(ERROR_INVALID_MAP_PATH);
-	core->texture_count = 0;
-	core->color_count = 0;
-	get_parser_infos();
-	print_parser_infos(core->parser_infos);
-	mlx_teste();
+	// core->texture_count = 0;
+	// core->color_count = 0;
+	// get_parser_infos();
+	map_builder();
 	clear_all();
 }
+// mlx_teste();
+// print_parser_infos(core->parser_infos);
 // constructor_map();
 // continua no proximo episodio
