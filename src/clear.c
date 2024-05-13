b639ea38b94c8bf4b1502b19f5a5095bbf44fdba
Arthur Sobreira <tutusobreira@gmail.com>
2024-05-13 17:21:41 -0300

    Update clear.c

--- src/clear.c
+++ src/clear.c
@@ -6,13 +6,13 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 21:54:25 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/05/13 13:58:43 by arsobrei         ###   ########.fr       */
+/*   Updated: 2024/05/13 17:21:20 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	ft_free_local_matrix(char **matrix)
+void	free_local_matrix(char **matrix)
 {
 	size_t	index;
 
@@ -25,17 +25,33 @@ void	ft_free_local_matrix(char **matrix)
 	}
 }
 
+void	clear_mlx(void)
+{
+	t_mlx	*mlx;
+
+	mlx = get_mlx();
+	if (mlx->img_ptr)
+		mlx_delete_image(mlx->win_ptr, mlx->img_ptr);
+	if (mlx->textures[NORTH])
+		mlx_delete_texture(mlx->textures[NORTH]);
+	if (mlx->textures[SOUTH])
+		mlx_delete_texture(mlx->textures[SOUTH]);
+	if (mlx->textures[WEST])
+		mlx_delete_texture(mlx->textures[WEST]);
+	if (mlx->textures[EAST])
+		mlx_delete_texture(mlx->textures[EAST]);
+	if (mlx->win_ptr)
+		mlx_terminate(mlx->win_ptr);
+	// free(mlx);
+}
+
 void	clear_all(void)
 {
 	t_cub3d	*core;
 
 	core = get_core();
-	ft_free_local_matrix(core->parser_infos);
+	clear_mlx();
+	free_local_matrix(core->parser_infos);
 	if (get_map()->map_str)
 		ft_free_matrix(get_map()->map_str);
-	if (get_mlx()->win_ptr)
-	{
-		mlx_terminate(get_mlx()->win_ptr);
-		free(get_mlx());
-	}
 }
