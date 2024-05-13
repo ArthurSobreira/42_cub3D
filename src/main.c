37bb1af668999de28424e902d477b656f409bcd8
Arthur Sobreira <tutusobreira@gmail.com>
2024-05-13 13:44:57 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 12:22:14 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/05/10 17:47:10 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/05/13 13:29:53 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,18 +27,24 @@ void	print_parser_infos(char **parser_infos)
 int	main(int argc, char *argv[])
 {
 	t_cub3d	*core;
+	t_mlx	*mlx;
 
 	core = get_core();
 	core->map_path = argv[1];
 	if (argc != 2 || !valid_map_name(core->map_path))
 		ft_error(ERROR_INVALID_MAP_PATH);
-	// core->texture_count = 0;
-	// core->color_count = 0;
-	// get_parser_infos();
+	core->texture_count = 0;
+	core->color_count = 0;
+	get_parser_infos();
 	map_builder();
+	mlx = init_mlx();
+	
+	render_background(mlx);
+	mlx_image_to_window(mlx->win_ptr, mlx->img_ptr, 0, 0);
+	mlx_loop(mlx->win_ptr);
+
+	// print_parser_infos(core->parser_infos);
+	// constructor_map();
 	clear_all();
 }
-// mlx_teste();
-// print_parser_infos(core->parser_infos);
-// constructor_map();
 // continua no proximo episodio
