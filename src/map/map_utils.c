17a36da3cdaff2cdf30ef4201633d2345f06b3e1
phenriq2 <phenriq2@student.42sp.org.br>
2024-05-14 13:50:13 -0300

    added a function that sets strings to 1 and then copies the map on top of it

--- src/map/map_utils.c
+++ src/map/map_utils.c
@@ -6,7 +6,7 @@
 /*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 10:51:41 by phenriq2          #+#    #+#             */
-/*   Updated: 2024/05/10 16:55:17 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/05/14 13:40:12 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,4 +66,26 @@ t_bool	is_map_line(char *line)
 		i++;
 	}
 	return (TRUE);
+}
+
+void	map_cpy(void)
+{
+	char	**map;
+	char	**map_cpy;
+	int		i;
+
+	map = get_map()->map_str;
+	map_cpy = ft_calloc(get_map()->max_y + 1, sizeof(char *));
+	i = 0;
+	while (map[i])
+	{
+		map_cpy[i] = ft_calloc(get_map()->max_x + 1, sizeof(char));
+		ft_memset(map_cpy[i], '1', get_map()->max_x);
+		map_cpy[i][get_map()->max_x] = '\0';
+		ft_memcpy(map_cpy[i], map[i], ft_strlen(map[i]));
+		map_cpy[i] = ft_replace(map_cpy[i], " ", "1");
+		i++;
+	}
+	ft_free_matrix(map);
+	get_map()->map_str = map_cpy;
 }
