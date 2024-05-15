06baba36c3d7e9373b196291b98742b139555948
phenriq2 <phenriq2@student.42sp.org.br>
2024-05-15 14:39:20 -0300

    new getter

--- src/utils/getters.c
+++ src/utils/getters.c
@@ -6,7 +6,7 @@
 /*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 11:14:17 by phenriq2          #+#    #+#             */
-/*   Updated: 2024/04/30 16:44:10 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/05/15 13:58:34 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,13 @@ t_cub3d	*get_core(void)
 t_mlx	*get_mlx(void)
 {
 	static t_mlx	core;
+
+	return (&core);
+}
+
+t_player	*get_player(void)
+{
+	static t_player	core;
 
 	return (&core);
 }
