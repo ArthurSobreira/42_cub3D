c1c1bf52a09971df7eee7fce20280ebad09b2cba
phenriq2 <phenriq2@student.42sp.org.br>
2024-05-15 14:38:06 -0300

    player move

--- src/keyhook.c
+++ src/keyhook.c
@@ -6,7 +6,7 @@
 /*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 16:55:53 by phenriq2          #+#    #+#             */
-/*   Updated: 2024/05/14 17:07:00 by phenriq2         ###   ########.fr       */
+/*   Updated: 2024/05/15 14:34:52 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,14 @@ void	my_keyhook(mlx_key_data_t keydata, void *param)
 	}
 	if ((keydata.key == UP || keydata.key == W) && (keydata.action == P
 			|| keydata.action == RE))
-		return ;
+		get_player()->y -= 4;
 	if ((keydata.key == RIGHT || keydata.key == D) && (keydata.action == P
 			|| keydata.action == RE))
-		return ;
+		get_player()->x += 4;
 	if ((keydata.key == LEFT || keydata.key == A) && (keydata.action == P
 			|| keydata.action == RE))
-		return ;
+		get_player()->x -= 4;
 	if ((keydata.key == DOWN || keydata.key == S) && (keydata.action == P
 			|| keydata.action == RE))
-		return ;
+		get_player()->y += 4;
 }
