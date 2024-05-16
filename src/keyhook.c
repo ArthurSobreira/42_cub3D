ec0832aafdd4239cebb49dd290ddb9d27bba726d
phenriq2 <phenriq2@student.42sp.org.br>
2024-05-16 11:23:07 -0300

    rotation functions have been moved to another file

--- src/keyhook.c
+++ src/keyhook.c
@@ -5,49 +5,26 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: phenriq2 <phenriq2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/14 16:55:53 by phenriq2          #+#    #+#             */
-/*   Updated: 2024/05/15 19:31:25 by phenriq2         ###   ########.fr       */
+/*   Created: 2024/05/16 10:58:52 by phenriq2          #+#    #+#             */
+/*   Updated: 2024/05/16 11:05:56 by phenriq2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
-static void	move_left(void)
-{
-	t_player	*player;
-
-	player = get_player();
-	player->degree -= 0.1;
-	if (player->degree < 0)
-		player->degree += TWO_PI;
-	player->dx = cos(player->degree);
-	player->dy = sin(player->degree);
-}
-
-static void	move_right(void)
-{
-	t_player	*player;
-
-	player = get_player();
-	player->degree += 0.1;
-	if (player->degree > TWO_PI)
-		player->degree -= TWO_PI;
-	player->dx = cos(player->degree);
-	player->dy = sin(player->degree);
-}
 
 static void	move_w(void)
 {
 	t_player	*player;
 
 	player = get_player();
-	if (player->x + player->dx * 4 < 0 || player->y + player->dy * 4 < 0)
+	if (player->x + player->dx * MOVE_SPEED < 0 || player->y + player->dy
+		* MOVE_SPEED < 0)
 		return ;
-	if (player->x + player->dx * 4 >= WINDOW_WIDTH || player->y + player->dy
-		* 4 >= WINDOW_HEIGHT)
+	if (player->x + player->dx * MOVE_SPEED >= WINDOW_WIDTH || player->y
+		+ player->dy * MOVE_SPEED >= WINDOW_HEIGHT)
 		return ;
-	player->x += player->dx * 4;
-	player->y += player->dy * 4;
+	player->x += player->dx * MOVE_SPEED;
+	player->y += player->dy * MOVE_SPEED;
 }
 
 static void	move_s(void)
@@ -55,41 +32,44 @@ static void	move_s(void)
 	t_player	*player;
 
 	player = get_player();
-	if (player->x - player->dx * 4 < 0 || player->y - player->dy * 4 < 0)
+	if (player->x - player->dx * MOVE_SPEED < 0 || player->y - player->dy
+		* MOVE_SPEED < 0)
 		return ;
-	if (player->x - player->dx * 4 >= WINDOW_WIDTH || player->y - player->dy
-		* 4 >= WINDOW_HEIGHT)
+	if (player->x - player->dx * MOVE_SPEED >= WINDOW_WIDTH || player->y
+		- player->dy * MOVE_SPEED >= WINDOW_HEIGHT)
 		return ;
-	player->x -= player->dx * 4;
-	player->y -= player->dy * 4;
+	player->x -= player->dx * MOVE_SPEED;
+	player->y -= player->dy * MOVE_SPEED;
 }
 
-void move_a(void)
+void	move_a(void)
 {
 	t_player	*player;
 
 	player = get_player();
-	if (player->x - player->dy * 4 < 0 || player->y + player->dx * 4 < 0)
+	if (player->x + player->dy * MOVE_SPEED < 0 || player->y - player->dx
+		* MOVE_SPEED < 0)
 		return ;
-	if (player->x - player->dy * 4 >= WINDOW_WIDTH || player->y + player->dx
-		* 4 >= WINDOW_HEIGHT)
+	if (player->x + player->dy * MOVE_SPEED >= WINDOW_WIDTH || player->y
+		- player->dx * MOVE_SPEED >= WINDOW_HEIGHT)
 		return ;
-	player->x += player->dy * 4;
-	player->y -= player->dx * 4;
+	player->x += player->dy * MOVE_SPEED;
+	player->y -= player->dx * MOVE_SPEED;
 }
 
-void move_d(void)
+void	move_d(void)
 {
 	t_player	*player;
 
 	player = get_player();
-	if (player->x + player->dy * 4 < 0 || player->y - player->dx * 4 < 0)
+	if (player->x - player->dy * MOVE_SPEED < 0 || player->y + player->dx
+		* MOVE_SPEED < 0)
 		return ;
-	if (player->x + player->dy * 4 >= WINDOW_WIDTH || player->y - player->dx
-		* 4 >= WINDOW_HEIGHT)
+	if (player->x - player->dy * MOVE_SPEED >= WINDOW_WIDTH || player->y
+		+ player->dx * MOVE_SPEED >= WINDOW_HEIGHT)
 		return ;
-	player->x -= player->dy * 4;
-	player->y += player->dx * 4;
+	player->x -= player->dy * MOVE_SPEED;
+	player->y += player->dx * MOVE_SPEED;
 }
 
 void	my_keyhook(mlx_key_data_t keydata, void *param)
@@ -103,13 +83,13 @@ void	my_keyhook(mlx_key_data_t keydata, void *param)
 		exit(EXIT_SUCCESS);
 	}
 	if (mlx_is_key_down(mlx->win_ptr, W) || mlx_is_key_down(mlx->win_ptr, UP))
-		move_w(); //-4
+		move_w();
 	if (mlx_is_key_down(mlx->win_ptr, S) || mlx_is_key_down(mlx->win_ptr, DOWN))
 		move_s();
 	if (mlx_is_key_down(mlx->win_ptr, RIGHT))
 		move_right();
 	if (mlx_is_key_down(mlx->win_ptr, LEFT))
-		move_left(); //-4
+		move_left();
 	if (mlx_is_key_down(mlx->win_ptr, A))
 		move_a();
 	if (mlx_is_key_down(mlx->win_ptr, D))
