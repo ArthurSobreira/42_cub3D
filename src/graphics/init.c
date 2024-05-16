feab0a59767c8cc17c986170ee552001fab228fa
Arthur Sobreira <tutusobreira@gmail.com>
2024-05-16 13:17:51 -0300

    Update init.c

--- src/graphics/init.c
+++ src/graphics/init.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 12:31:13 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/05/16 12:26:16 by arsobrei         ###   ########.fr       */
+/*   Updated: 2024/05/16 13:16:46 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,21 +32,20 @@ t_mlx	*init_mlx(void)
 		ft_error(ERROR_MLX_INIT);
 	get_color(mlx, CEIL);
 	get_color(mlx, FLOOR);
-	init_player();
 	return (mlx);
 }
 
 void	init_player(void)
 {
 	set_player_direction();
+	if (get_player()->direction == 'N')
+		get_player()->angle = THREE_PI_OVER_TWO;
+	else if (get_player()->direction == 'S')
+		get_player()->angle = PI_OVER_TWO;
+	else if (get_player()->direction == 'W')
+		get_player()->angle = PI;
+	else if (get_player()->direction == 'E')
+		get_player()->angle = TWO_PI;
 	get_player()->delta_x = cos(get_player()->angle);
 	get_player()->delta_y = sin(get_player()->angle);
-	if (get_player()->direction == 'N')
-		get_player()->angle = PI_270;
-	else if (get_player()->direction == 'S')
-		get_player()->angle = HALF_PI;
-	else if (get_player()->direction == 'W')
-		get_player()->angle = TWO_PI;
-	else if (get_player()->direction == 'E')
-		get_player()->angle = 0;
 }
