e03fd296271a56cad58f4c54e9d17c76ac7c24f2
Arthur Sobreira <tutusobreira@gmail.com>
2024-05-17 12:20:52 -0300

    Update draw_line.c

--- src/graphics/draw_line.c
+++ src/graphics/draw_line.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 13:28:08 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/05/17 11:23:45 by arsobrei         ###   ########.fr       */
+/*   Updated: 2024/05/17 12:13:58 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,12 @@ static void	plot_thick_pixel(t_point point, int thickness)
 		j = -thickness / 2;
 		while (j <= thickness / 2)
 		{
-			if ((point.x + i >= 0) && (point.y + j >= 0) && \
-				(point.x + i < WINDOW_WIDTH) && \
-				(point.y + j < WINDOW_HEIGHT))
+			if ((point.coord_x + i >= 0) && (point.coord_y + j >= 0) && \
+				(point.coord_x + i < WINDOW_WIDTH) && \
+				(point.coord_y + j < WINDOW_HEIGHT))
 			{
-				mlx_put_pixel(get_mlx()->img_ptr, point.x + i, \
-					point.y + j, point.color);
+				mlx_put_pixel(get_mlx()->img_ptr, point.coord_x + i, \
+					point.coord_y + j, point.color);
 			}
 			j++;
 		}
@@ -41,18 +41,18 @@ void	bresenham(t_point inital_point, t_point end_point, int thickness)
 	float	temp_delta_x;
 	float	temp_delta_y;
 
-	temp_delta_x = end_point.x - inital_point.x;
-	temp_delta_y = end_point.y - inital_point.y;
+	temp_delta_x = end_point.coord_x - inital_point.coord_x;
+	temp_delta_y = end_point.coord_y - inital_point.coord_y;
 	if (fabs(temp_delta_x) > fabs(temp_delta_y))
 	{
-		if (inital_point.x > end_point.x)
+		if (inital_point.coord_x > end_point.coord_x)
 			bresenham_high(end_point, inital_point, thickness);
 		else
 			bresenham_high(inital_point, end_point, thickness);
 	}
 	else
 	{
-		if (inital_point.y > end_point.y)
+		if (inital_point.coord_y > end_point.coord_y)
 			bresenham_low(end_point, inital_point, thickness);
 		else
 			bresenham_low(inital_point, end_point, thickness);
@@ -65,7 +65,7 @@ void	bresenham_high(t_point initial_point, t_point end_point, int thickness)
 
 	init_bres(&bres, initial_point, end_point);
 	bres.decision = (2 * bres.delta_y) - bres.delta_x;
-	while (bres.initial_x <= end_point.x)
+	while (bres.initial_x <= end_point.coord_x)
 	{
 		plot_thick_pixel((t_point){bres.initial_x, bres.initial_y, \
 			initial_point.color}, thickness);
@@ -86,7 +86,7 @@ void	bresenham_low(t_point initial_point, t_point end_point, int thickness)
 
 	init_bres(&bres, initial_point, end_point);
 	bres.decision = (2 * bres.delta_x) - bres.delta_y;
-	while (bres.initial_y <= end_point.y)
+	while (bres.initial_y <= end_point.coord_y)
 	{
 		plot_thick_pixel((t_point){bres.initial_x, bres.initial_y, \
 			initial_point.color}, thickness);
