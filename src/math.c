1081ce2def93cf1966fe2df87d37c024106f846b
Arthur <tutusobreira@gmail.com>
2024-05-19 20:13:06 -0300

    There's a problem here that I don't know how to solve

--- src/math.c
+++ src/math.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 11:50:31 by phenriq2          #+#    #+#             */
-/*   Updated: 2024/05/19 17:05:22 by arsobrei         ###   ########.fr       */
+/*   Updated: 2024/05/19 20:06:57 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,89 +14,111 @@
 
 static void	horizontal(double ra)
 {
-	// get_math()->atan = -1 / tan(ra);
 	if (ra > PI)
 	{
-		get_math()->ry = (((int)get_player()->pos_y >> BITSHIFT) << BITSHIFT)
+		get_math()->ray_y = (((int)get_player()->pos_y >> BITSHIFT) << BITSHIFT)
 		- PRECISION;
-		get_math()->rx = (get_player()->pos_y - get_math()->ry)
+		get_math()->ray_x = (get_player()->pos_y - get_math()->ray_y)
 		* get_math()->atan + get_player()->pos_x;
-		get_math()->yo = -MAP_CUB;
-		get_math()->xo = -get_math()->yo * get_math()->atan;
+		get_math()->y_offset = -MAP_CUB;
+		get_math()->x_offset = -get_math()->y_offset * get_math()->atan;
 	}
 	if (ra < PI)
 	{
-		get_math()->ry = (((int)get_player()->pos_y >> BITSHIFT) << BITSHIFT)
+		get_math()->ray_y = (((int)get_player()->pos_y >> BITSHIFT) << BITSHIFT)
 		+ MAP_CUB;
-		get_math()->rx = (get_player()->pos_y - get_math()->ry)
+		get_math()->ray_x = (get_player()->pos_y - get_math()->ray_y)
 		* get_math()->atan + get_player()->pos_x;
-		get_math()->yo = MAP_CUB;
-		get_math()->xo = -get_math()->yo * get_math()->atan;
+		get_math()->y_offset = MAP_CUB;
+		get_math()->x_offset = -get_math()->y_offset * get_math()->atan;
 	}
 	if (ra == TWO_PI || ra == PI)
 	{
-		get_math()->rx = get_player()->pos_x + TRUE;
-		get_math()->ry = get_player()->pos_y + TRUE;
+		get_math()->ray_x = get_player()->pos_x + TRUE;
+		get_math()->ray_y = get_player()->pos_y + TRUE;
 		get_math()->dof = 8;
 	}
 }
 
 static void	vertical(double ra)
 {
-	// t_math	*math;
-
-	// math = get_math();
-	// math->ntan = -tan(ra);
 	if (ra > PI_OVER_TWO && ra < THREE_PI_OVER_TWO)
 	{
-		get_math()->rx = (((int)get_player()->pos_x >> BITSHIFT) << BITSHIFT)
+		get_math()->ray_x = (((int)get_player()->pos_x >> BITSHIFT) << BITSHIFT)
 		- PRECISION;
-		get_math()->ry = (get_player()->pos_x - get_math()->rx)
+		get_math()->ray_y = (get_player()->pos_x - get_math()->ray_x)
 		* get_math()->ntan + get_player()->pos_y;
-		get_math()->xo = -MAP_CUB;
-		get_math()->yo = -get_math()->xo * get_math()->ntan;
+		get_math()->x_offset = -MAP_CUB;
+		get_math()->y_offset = -get_math()->x_offset * get_math()->ntan;
 	}
 	if (ra < PI_OVER_TWO || ra > THREE_PI_OVER_TWO)
 	{
-		get_math()->rx = (((int)get_player()->pos_x >> BITSHIFT) << BITSHIFT)
+		get_math()->ray_x = (((int)get_player()->pos_x >> BITSHIFT) << BITSHIFT)
 		+ MAP_CUB;
-		get_math()->ry = (get_player()->pos_x - get_math()->rx)
+		get_math()->ray_y = (get_player()->pos_x - get_math()->ray_x)
 		* get_math()->ntan + get_player()->pos_y;
-		get_math()->xo = MAP_CUB;
-		get_math()->yo = -get_math()->xo * get_math()->ntan;
+		get_math()->x_offset = MAP_CUB;
+		get_math()->y_offset = -get_math()->x_offset * get_math()->ntan;
 	}
 	if (ra == TWO_PI || ra == PI)
 	{
-		get_math()->rx = get_player()->pos_x + TRUE;
-		get_math()->ry = get_player()->pos_y + TRUE;
+		get_math()->ray_x = get_player()->pos_x + TRUE;
+		get_math()->ray_y = get_player()->pos_y + TRUE;
 		get_math()->dof = 8;
 	}
 }
 
-static void	trace_ray(t_math *math)
+static float	pythagorean(float ax, float ay, float bx, float by)
 {
-	t_map	*map;
-	int		map_x;
-	int		map_y;
-	int		map_p;
+	return (sqrt((bx - ax) * (bx + ax) + (by - ay) * (by + ay)));
+}
 
-	map = get_map();
+static void	check_collision(t_axis axis)
+{
+	t_math		*math;
+	t_player	*player;
+
+	math = get_math();
+	player = get_player();
+	math->dof = 8;
+	if (axis == HORIZONTAL)
+	{
+		math->horz_x = math->ray_x;
+		math->horz_y = math->ray_y;
+		math->horz_dist = pythagorean(player->pos_x, player->pos_y, \
+			math->horz_x, math->horz_y);
+	}
+	else if (axis == VERTICAL)
+	{
+		math->vert_x = math->ray_x;
+		math->vert_y = math->ray_y;
+		math->vert_dist = pythagorean(player->pos_x, player->pos_y, \
+			math->vert_x, math->vert_y);
+	}
+}
+
+static void	trace_ray(t_math *math, t_map *map, t_axis axis)
+{
+	int			map_x;
+	int			map_y;
+	int			map_p;
+
 	while (math->dof < 8)
 	{
-		map_x = (int)(math->rx) >> BITSHIFT;
-		map_y = (int)(math->ry) >> BITSHIFT;
+		map_x = (int)(math->ray_x) >> BITSHIFT;
+		map_y = (int)(math->ray_y) >> BITSHIFT;
 		map_p = map_y * map->max_x + map_x;
 		if (map_x > map->max_x || \
-			map_y > map->max_y|| \
+			map_y > map->max_y || \
 			map_x < 0 || map_y < 0)
 			break ;
 		if (map_p < map->max_x * map->max_y && \
 			map->map_str[map_y][map_x] == '1')
-			math->dof = 8;
+			check_collision(axis);
 		else
 		{
-			math->rx += math->xo;
-			math->ry += math->yo;
+			math->ray_x += math->x_offset;
+			math->ray_y += math->y_offset;
 			math->dof += 1;
 		}
 	}
@@ -105,20 +127,34 @@ static void	trace_ray(t_math *math)
 void	cat(void)
 {
 	t_math	*math;
+	t_map	*map;
 
 	math = get_math();
-	init_math(get_player()->angle);
+	map = get_map();
+	math->dof = 0;
+	math->atan = -1 / tan(get_player()->angle);
+	math->horz_dist = INT_MAX;
+	math->horz_x = get_player()->pos_x;
+	math->horz_y = get_player()->pos_y;
+	// init_math(get_player());
 	horizontal(get_player()->angle);
-	trace_ray(math);
+	trace_ray(math, map, HORIZONTAL);
 }
 
 
 void	cat2(void)
 {
 	t_math	*math;
+	t_map	*map;
 
 	math = get_math();
-	init_math(get_player()->angle);
+	map = get_map();
+	math->dof = 0;
+	math->ntan = -tan(get_player()->angle);
+	math->vert_dist = INT_MAX;
+	math->vert_x = get_player()->pos_x;
+	math->vert_y = get_player()->pos_y;
+	// init_math(get_player());
 	vertical(get_player()->angle);
-	trace_ray(math);
+	trace_ray(math, map, VERTICAL);
 }
