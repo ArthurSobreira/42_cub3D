173eeda436b3b37fa193a05b5140507216d4ed9b
Arthur Sobreira <tutusobreira@gmail.com>
2024-05-20 16:25:00 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 12:22:14 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/05/20 15:43:41 by arsobrei         ###   ########.fr       */
+/*   Updated: 2024/05/20 16:04:29 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(int argc, char *argv[])
 	t_cub3d	*core;
 
 	core = get_core();
-	setup_cub(core, argv);
+	init_cub(core, argv);
 	if (argc != 2 || !valid_map_name(core->map_path))
 		ft_error(ERROR_INVALID_MAP_PATH);
 	core->get_parser_infos();
