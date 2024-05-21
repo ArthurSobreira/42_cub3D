abdabe5d216d1517e8c796bb7a636ae301cc6bbd
Arthur Sobreira <tutusobreira@gmail.com>
2024-05-20 23:52:46 -0300

    Update ray_utils.c

--- src/math/ray_utils.c
+++ src/math/ray_utils.c
@@ -1,16 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   math_utils.c                                       :+:      :+:    :+:   */
+/*   ray_utils.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 15:14:58 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/05/20 15:21:57 by arsobrei         ###   ########.fr       */
+/*   Updated: 2024/05/20 23:52:11 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+void	draw_rays(t_math *math, t_player *player)
+{
+	t_point	initial_point;
+	t_point	final_point;
+	short	thickness;
+	
+	if ((!get_core()->draw_map) || (!get_core()->draw_rays))
+		return ;
+	thickness = 1;
+	initial_point.coord_x = player->pos_x + DIRECTION_OFFSET;
+	initial_point.coord_y = player->pos_y + DIRECTION_OFFSET;
+	final_point.coord_x = math->ray_x;
+	final_point.coord_y = math->ray_y;
+	initial_point.color = COLOR_PLAYER;
+	final_point.color = COLOR_PLAYER;
+	bresenham(initial_point, final_point, thickness);
+}
 
 void	trace_ray(t_math *math, t_map *map, t_axis axis)
 {
@@ -60,4 +78,12 @@ void	check_wall_collision(t_axis axis)
 		math->vert_dist = hypot(math->vert_x - player->pos_x, \
 			math->vert_y - player->pos_y);
 	}
+}
+
+void	normalize_angle(double *angle)
+{
+	if (*angle < 0)
+		*angle += TWO_PI;
+	if (*angle > TWO_PI)
+		*angle -= TWO_PI;
 }
