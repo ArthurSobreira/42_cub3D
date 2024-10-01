9ac2e5d1ef075afeedc62117506778165af3f3c4
Arthur Sobreira <tutusobreira@gmail.com>
2024-10-01 17:31:19 -0300

    Update main_bonus.c

--- src/bonus/main_bonus.c
+++ src/bonus/main_bonus.c
@@ -6,11 +6,11 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 12:22:14 by arsobrei          #+#    #+#             */
-/*   Updated: 2024/05/28 12:37:56 by arsobrei         ###   ########.fr       */
+/*   Updated: 2024/10/01 17:25:15 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cub3d_bonus.h"
+#include "cub3d.h"
 
 int	main(int argc, char *argv[])
 {
